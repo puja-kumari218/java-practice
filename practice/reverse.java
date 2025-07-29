aba0cd86b089183cbc8f9783c7f7422fc9012665
Puja Kumari <pujakumarijb@gmail.com>
2025-07-29 22:01:44 +0530

    practice prime

---
+++ practice/reverse.java
@@ -0,0 +1,13 @@
+public class reverse {
+    public static void main(String[] args) {
+    int num =12345;
+    int result =0;
+    while (num>0) {
+        int remainder = num % 10;
+        result = result * 10 + remainder;
+        num = num/10;
+
+        
+    }System.out.println(result);
+    }
+}
